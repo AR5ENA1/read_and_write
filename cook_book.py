654c46696a7d56273a8acb95609c9cff78393a11
Pavel Lutoshkin <pashokrmo@mail.ru>
2022-05-03 22:19:03 +0500

    Task_â2

--- cook_book.py
+++ cook_book.py
@@ -13,4 +13,17 @@ with open('recipe.txt', 'r', encoding='utf-8') as file:
                     dict(ingredient_name=ingredient[0], quantity=int(ingredient[1]), measure=ingredient[2])]
         else:
             key = None
-print(cook_book)
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dishe in dishes:
+        for ingredient in cook_book[dishe]:
+            if ingredient['ingredient_name'] in shop_list:
+                shop_list[ingredient['ingredient_name']]['quantity'] += ingredient['quantity'] * person_count
+            else:
+                shop_list[ingredient['ingredient_name']] = {'measure': ingredient['measure'],
+                                                            'quantity': ingredient['quantity'] * person_count}
+    return print(shop_list)
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет', 'Фахитос', 'Утка по-пекински'], 2)
